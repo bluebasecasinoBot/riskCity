99f221700f1600e6d35904f957d1caf1d6be840e
bluebasecasinoBot <developerme33@gmail.com>
2024-09-04 04:38:35 -0700

    Update mainPage.js

--- 3d_web3site/mainPage.js
+++ 3d_web3site/mainPage.js
@@ -90,7 +90,7 @@ __SYD.main_front = () =>{
                         'video',
                         {
                             style:'width:70%;max-width:70%;height:auto;z-index:500',//height:calc(100% - 80px);
-                            autoplay:true,preload:'auto'
+                            autoplay:true,preload:'auto' , playsinline:true
                         },
                         [
                             __c('source',{src:'./memeVideo.mp4'},[],{type:'vid_src'})
